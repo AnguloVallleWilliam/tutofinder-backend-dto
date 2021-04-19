1bdb86e93499ab0ad4ddd331299d14c991665777
William <wmavalle@gmail.com>
2021-04-19 01:52:41 -0500

    "Test"

---
+++ src/test/java/com/tutofinder/app/service/AlumnoServiceTest.java
@@ -0,0 +1,23 @@
+package com.tutofinder.app.service;
+
+import com.tutofinder.app.dto.CursoDto;
+import com.tutofinder.app.dto.create.CreateCursoDto;
+import com.tutofinder.app.exception.BookingException;
+import com.tutofinder.app.response.BookingResponse;
+import com.tutofinder.app.services.CursoService;
+import com.tutofinder.app.services.TarjetaService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AlumnoServiceTest {
+
+}
